d9c21b02730985bce8b1a740725e28c7f39fb826
HanxuLiu <liu_han_xu@163.com>
2024-03-21 14:22:02 +0800

    update code of ptrace

--- test_ptrace/test_fork/test-fork.c
+++ test_ptrace/test_fork/test-fork.c
@@ -4,17 +4,17 @@
 #include <unistd.h>
 
 int main()
-{  
-    pid_t child;
-    child = fork();
+{
+    pid_t child_pid = fork();
     if(child == 0) // child process
     {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        printf("this message will not print\n");
         printf("child to call execl, run ls command\n");
         execl("/usr/bin/ls", "ls", NULL); // run ls and send signal
     } else { // parent process
         wait(NULL); // wait for child process signal
-        ptrace(PTRACE_CONT, child, NULL, NULL); // tell child to continue
+        ptrace(PTRACE_CONT, child_pid, NULL, NULL); // tell child to continue
         printf("after child continue, parent exit\n");
     }
     return 0;
